7464a332667be55febecaeade30746cbb499d495
yongckim <zxcv9203@naver.com>
2022-04-05 16:29:32 +0900

    spring basic part3

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(getMemberRepository());
     }
 
-    private MemoryMemberRepository getMemberRepository() {
+    @Bean
+    public MemoryMemberRepository getMemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 getMemberRepository(), getDiscountPolicy());
     }
 
+    @Bean
     public DiscountPolicy getDiscountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
